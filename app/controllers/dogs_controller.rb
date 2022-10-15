bab9cfc37f2454a3675f6cdc6305976190d33346
Chemobo <jkchemobo@gmail.com>
2022-10-15 14:14:46 +0300

    CRUD operations for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,26 @@
 class DogsController < ApplicationController
+    def index
+        dogs = Dog.all
+        render json: dogs
+    end
+
+    def show
+        dog = Dog.find(params[:id])
+        render json: dog
+    end 
+    def create
+        dog = @current_user.dogs.create!(cocktail_params)
+        render json: dog, status: :created
+    end
+
+    def destroy
+        dog = Dog.find(params[:id])
+        dog.destroy
+        head :no_content
+    end
+
+    private
+    def dog_params
+        params.permit(:name, :image, :description)
+    end
 end
