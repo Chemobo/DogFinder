980a53a7bd4493a1d5068f1cbc846829d22da099
Chemobo <jkchemobo@gmail.com>
2022-10-15 14:07:58 +0300

    configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :users
+  resources :dogs, only: [:index, :show, :create, :destroy]
+  resources :users,  only: [:index, :show, :create]
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  root :to => 'index#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
