8474fe80511527a2297fbac69cefe91e482bed41
Chemobo <jkchemobo@gmail.com>
2022-10-15 14:01:46 +0300

    sets up relationships

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,6 @@
 class Dog < ApplicationRecord
+    belongs_to :user
+    
+    validates :name, presence: true
+    validates :description, length: {minimum: 50}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :dogs
+    
+    has_secure_password
+    validates :user_name, presence: true, uniqueness: true 
+    validates :name, presence: true
 end
